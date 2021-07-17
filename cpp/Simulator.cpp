577dd2e8fe3bdb9251d4d85a8d658f297d860b92
Og Astorga <ogastorga@chromium.org>
2021-07-17 18:35:17 -0500

    Simulator base

---
+++ cpp/Simulator.cpp
@@ -0,0 +1,9 @@
+// Copyright 2021 Og Astorga. All rights reserved.
+// Use of this source code is governed by a CC-BY-NC-SA license that can be
+// found in the LICENSE file.
+
+#include "Simulator.h"
+
+Simulator::Simulator() {}
+
+Simulator::~Simulator() = default;
---
+++ cpp/Simulator.h
@@ -0,0 +1,15 @@
+// Copyright 2021 Og Astorga. All rights reserved.
+// Use of this source code is governed by a CC-BY-NC-SA license that can be
+// found in the LICENSE file.
+
+#ifndef CPP_SIMULATOR_H_
+#define CPP_SIMULATOR_H_
+
+class Simulator {
+public:
+    Simulator();
+    ~Simulator();
+private:
+};
+
+#endif  // CPP_SIMULATOR_H_
