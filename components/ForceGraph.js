9f65fae9c981276eb4e40dcac1d8cc3c4e7a4bbb
Og Astorga <ogastorga@chromium.org>
2021-07-20 12:17:01 -0500

    show node and link labels

--- components/ForceGraph.js
+++ components/ForceGraph.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef, Fragment } from 'react';
 import ForceGraph2D from 'react-force-graph-2d';
 
-const NODE_R = 5;
+const NODE_R = 7;
 const ForceGraph = ({ dijkstra }) => {
     const [graphData, SetGraphData] = useState(null);
     const [source, setSource] = useState("");
@@ -49,7 +49,7 @@ const ForceGraph = ({ dijkstra }) => {
             ["E", "N", 3.1],
         ];
 
-        const nodes = [];
+        let nodes = [];
         const links = [];
 
         const nodeDict = {};
@@ -67,11 +67,13 @@ const ForceGraph = ({ dijkstra }) => {
             links.push({
                 source: row[0],
                 target: row[1],
+                label: row[2].toFixed(2),
             });
 
             links.push({
                 source: row[1],
                 target: row[0],
+                label: row[2].toFixed(2),
             });
 
             dijkstra.current.AddEdge(row[0], row[1], row[2]);
@@ -111,7 +113,7 @@ const ForceGraph = ({ dijkstra }) => {
         <Fragment>
             <div style={{ position: 'absolute', zIndex: 10 }}>
                 <div>
-                    <select value={source} onChange={e => setSource(e.target.value)}>
+                    <select style={{ padding: 10 }} value={source} onChange={e => setSource(e.target.value)}>
                         <option>-- Origen --</option>
                         {graphData.nodes.map(node => (
                             <option key={node.id} value={node.id}>{node.id}</option>
@@ -119,7 +121,7 @@ const ForceGraph = ({ dijkstra }) => {
                     </select>
                 </div>
                 <div>
-                    <select value={target} onChange={e => setTarget(e.target.value)}>
+                    <select style={{ padding: 10 }} value={target} onChange={e => setTarget(e.target.value)}>
                         <option>-- Destino --</option>
                         {graphData.nodes.map(node => (
                             <option key={node.id} value={node.id}>{node.id}</option>
@@ -127,22 +129,97 @@ const ForceGraph = ({ dijkstra }) => {
                     </select>
                 </div>
             </div>
+
+            {/* nodeCanvasObjectMode={node => highlightNodes.has(node.id) ? 'before' : undefined} */}
             <ForceGraph2D
                 ref={fgRef}
                 graphData={graphData}
                 nodeRelSize={NODE_R}
                 autoPauseRedraw={false}
                 linkWidth={link => highlightLinks.has(`${link.source.id}-${link.target.id}`) ? 5 : 1}
-                linkDirectionalParticles={4}
+                linkDirectionalParticles={2}
                 linkDirectionalParticleWidth={link => highlightLinks.has(`${link.source.id}-${link.target.id}`) ? 4 : 0}
-                nodeCanvasObjectMode={node => highlightNodes.has(node.id) ? 'before' : undefined}
                 nodeCanvasObject={(node, ctx) => {
                     // add ring just for highlighted nodes
+                    if (highlightNodes.has(node.id)) {
+                        ctx.beginPath();
+                        ctx.arc(node.x, node.y, NODE_R * 1.4, 0, 2 * Math.PI, false);
+                        ctx.fillStyle = 'orange';
+                        ctx.fill();
+                    }
+
                     ctx.beginPath();
-                    ctx.arc(node.x, node.y, NODE_R * 1.4, 0, 2 * Math.PI, false);
-                    ctx.fillStyle = 'orange';
+                    ctx.arc(node.x, node.y, NODE_R, 0, 2 * Math.PI, false);
+                    ctx.fillStyle = 'steelblue';
                     ctx.fill();
+
+                    const label = node.id;
+                    const fontSize = 6;
+                    ctx.font = `${fontSize}px Sans-Serif`;
+                    const textWidth = ctx.measureText(label).width;
+                    const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
+
+                    ctx.textAlign = 'center';
+                    ctx.textBaseline = 'middle';
+                    ctx.fillStyle = 'white';
+                    ctx.fillText(label, node.x, node.y);
+        
+                    node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
                 }}
+                linkCanvasObjectMode={() => 'after'}
+                linkCanvasObject={(link, ctx) => {
+                    const MAX_FONT_SIZE = 4;
+                    const LABEL_NODE_MARGIN = 11;
+
+                    const start = link.source;
+                    const end = link.target;
+
+                    if (start.id > end.id) {
+                        return;
+                    }
+
+                    // ignore unbound links
+                    if (typeof start !== 'object' || typeof end !== 'object') return;
+
+                    // calculate label positioning
+                    const textPos = Object.assign(...['x', 'y'].map(c => ({
+                      [c]: start[c] + (end[c] - start[c]) / 2 // calc middle point
+                    })));
+
+                    const relLink = { x: end.x - start.x, y: end.y - start.y };
+
+                    const maxTextLength = Math.sqrt(Math.pow(relLink.x, 2) + Math.pow(relLink.y, 2)) - LABEL_NODE_MARGIN * 2;
+
+                    let textAngle = Math.atan2(relLink.y, relLink.x);
+                    // maintain label vertical orientation for legibility
+                    if (textAngle > Math.PI / 2) textAngle = -(Math.PI - textAngle);
+                    if (textAngle < -Math.PI / 2) textAngle = -(-Math.PI - textAngle);
+
+                    const label = link.label;
+
+                    // estimate fontSize to fit in link length
+                    ctx.font = '1px Sans-Serif';
+                    const fontSize = Math.min(MAX_FONT_SIZE, maxTextLength / ctx.measureText(label).width);
+                    ctx.font = `${fontSize}px Sans-Serif`;
+                    const textWidth = ctx.measureText(label).width;
+                    const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
+
+                    // draw text label (with background rect)
+                    ctx.save();
+                    ctx.translate(textPos.x, textPos.y);
+                    ctx.rotate(textAngle);
+
+                    ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+                    ctx.fillRect(- bckgDimensions[0] / 2, - bckgDimensions[1] / 2, ...bckgDimensions);
+
+                    ctx.textAlign = 'center';
+                    ctx.textBaseline = 'middle';
+                    ctx.fillStyle = 'darkgrey';
+                    ctx.fillText(label, 0, 0);
+                    ctx.restore();
+                }}
+                cooldownTicks={100}
+                onEngineStop={() => fgRef.current.zoomToFit(400, 200)}
             />
         </Fragment>
     )
